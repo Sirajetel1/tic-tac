cec1cebb2da725f3eed48d447ffc48bae27cb015
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-12-20 15:43:09 +0000

    third commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+
+
+// trying to link java with HTML Iinerms of inuts.
+// get inputs to and it will help to add squares
+
+var square = document.gotElementById("input");
+square.addEventListener("click",function(event){
+  event.playerTurn();
+})
+//////////////////////
+// function square() ****
+// // playerTurn
+ var playerTurn P1=X ,P2=O;
+// // switch player a to player b
+switch(expression) {
+  case 1:
+    Player= P1
+    break;
+  case 2 :
+    Player= P2
+    break;
+
+//     //  I'am thinking to generate a loop for the switch
+// }
+//
+//
+// // thats for my loop wining and losing run
+var winningcombinations = [[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]]
+if (winningcombinations=X) {
+  alert("you win")
+}else("lose")
+
+if (winningcombinations=O) {
+  alert("you win")
+}else {
+  ("you lose")
+}
+// var Diagonal d1 =[3,5,7] || [7,5,3], d2=[1,5,9]||[9,5,1];
+// var horizontal h-1= [1,2,3]||[3,2,1] , h-2=[4,5,6]||[6,5,4],h-3=[7,8,9],[9,8,7];
+//
+// var vertical v1=[7,4,1]||[1,4,7], v2=[8,5,2]||[2,5,8],v3[9,6,3]||[3,6,9]
+//
+// function tiktak(x,y){
+// if (true) {
